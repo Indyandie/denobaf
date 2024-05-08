fe5db8cbecd5b8a6468b7e476b828f73bbcf5151
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-05-08 00:00:45 -0500

    style: format

--- requests.js
+++ requests.js
@@ -1,16 +1,14 @@
 import { log } from '../widgets/main.js'
 
-let resp = await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+let resp = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
 
 // console.log(resp)
 // console.log(resp.headers)
-log("content-type", resp.headers.get("Content-Type"))
+log('content-type', resp.headers.get('Content-Type'))
 // console.log(await resp.text())
 
-
-resp = await fetch("https://example.com");
-log("arrayBuffer", await resp.arrayBuffer())
-
+resp = await fetch('https://example.com')
+log('arrayBuffer', await resp.arrayBuffer())
 
 // stream response body
 
@@ -20,46 +18,44 @@ for await (const chunk of resp.body!) {
   console.log("chunk", chunk);
 } */
 
-fetch("https://example.com")
+fetch('https://example.com')
   .then(async (resp) => {
-    const reader = resp.body.getReader();
+    const reader = resp.body.getReader()
     while (true) {
-      const { done, value } = await reader.read();
+      const { done, value } = await reader.read()
       if (done) {
-        break;
+        break
       }
       const decodeValue = new TextDecoder().decode(value)
-      log("chunk", value );
-      log("decode-chunk",  decodeValue);
+      log('chunk', value)
+      log('decode-chunk', decodeValue)
     }
   })
   .catch((error) => {
-    console.error("Fetch error:", error);
-  });
-
+    console.error('Fetch error:', error)
+  })
 
 // request: method, headers, body
-const body = `{"name": "Deno"}`;
-resp = await fetch("https://example.com", {
-  method: "POST",
+const body = `{"name": "Deno"}`
+resp = await fetch('https://example.com', {
+  method: 'POST',
   headers: {
-    "Content-Type": "application/json",
-    "X-API-Key": "foobar",
+    'Content-Type': 'application/json',
+    'X-API-Key': 'foobar',
   },
   body,
-});
+})
 
 const resp_info = {
   method: resp.status,
   headers: resp.headers,
   body: await resp.text(),
 }
-log("basic-request", await resp_info)
-  
-const req = new Request("https://example.com", {
-  method: "GET",
-});
-resp = await fetch(req);
+log('basic-request', await resp_info)
 
-log("Request", resp)
+const req = new Request('https://example.com', {
+  method: 'GET',
+})
+resp = await fetch(req)
 
+log('Request', resp)
